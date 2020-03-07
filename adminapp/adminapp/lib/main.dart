aa8dc7277ebdef3c338164d9933a2df8ea220b33
Aubrey Malabie <malengatiger@gmail.com>
2020-03-07 02:59:51 +0200

    🌈 🌈 initial commit

---
+++ adminapp/adminapp/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:adminapp/bloc/AdminBloc.dart';
+import 'package:adminapp/ui/dashboard.dart';
+import 'package:adminapp/ui/onboard.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:stokvelibrary/bloc/theme.dart';
+import 'package:stokvelibrary/slide_right.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  var themeIndex;
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AdminBloc>.value(
+          value: AdminBloc(),
+        ),
+
+      ],
+      child: StreamBuilder<int>(
+          initialData: themeIndex == null ? 0 : themeIndex,
+          stream: themeBloc.newThemeStream,
+          builder: (context, snapShot) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Anchor',
+              theme: snapShot.data == null
+                  ? ThemeUtil.getTheme(themeIndex: themeIndex)
+                  : ThemeUtil.getTheme(themeIndex: snapShot.data),
+              home: MyHomePage(),
+            );
+          }),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+  void _checkAuth() async {
+    
+    var isAuthed = await adminBloc.isAuthenticated();
+    if (!isAuthed) {
+      Navigator.push(context, SlideRightRoute(widget: OnBoard()));
+      return;
+    } else {
+      Navigator.push(context, SlideRightRoute(widget: Dashboard()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('ADMIN'),
+      ),
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
